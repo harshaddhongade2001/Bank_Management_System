5c715756f8ed1760835402fca4ff09aa248d7db6
harshaddhongade2001 <harshaddhongade2001@gmail.com>
2023-10-30 15:31:02 +0530

    ok

--- src/Bank_Management_System/Signup2.java
+++ src/Bank_Management_System/Signup2.java
@@ -1,4 +1,14 @@
 package Bank_Management_System;
 
-public class Signup2 {
+import javax.swing.*;
+
+public class Signup2 extends JFrame {
+    Signup2(){
+        setSize(500,500);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+      new Signup2();
+    }
 }
